b750cdc02ea5ce8a6d8db66d7a3172678c9a6c6a
Vicent Paya <vicentpayaginer@gmail.com>
2024-08-09 20:04:08 +0200

    fix: quantity of elements in pageResponse fixed

--- infofarm/src/main/java/com/infofarm/common/dto/PageResponseDTO.java
+++ infofarm/src/main/java/com/infofarm/common/dto/PageResponseDTO.java
@@ -1,11 +1,9 @@
 package com.infofarm.common.dto;
 
-import lombok.Builder;
-
 import java.util.List;
 
 public record PageResponseDTO<T>(
         List<T> content,
-        int totalPages
+        long totalElements
 ) {
 }
